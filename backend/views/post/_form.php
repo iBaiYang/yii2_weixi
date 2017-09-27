91cd63d4a89852856dab8fc73a2d2f5f15279d54
ZhengJY <zhengjunyuan@peilian.com>
2017-09-27 21:05:09 +0800

    文章标签功能增加

--- backend/views/post/_form.php
+++ backend/views/post/_form.php
@@ -49,10 +49,6 @@ use common\models\Adminuser;
         ['prompt' => '请选择状态']
     ); ?>
 
-    <?= $form->field($model, 'create_time')->textInput() ?>
-
-    <?= $form->field($model, 'update_time')->textInput() ?>
-
     <?= $form->field($model, 'author_id')->dropDownList(
         Adminuser::find()
             ->select(['nickname', 'id'])
