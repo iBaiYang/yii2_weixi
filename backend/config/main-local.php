2853e0003f668e431559c010d0139f1c41479529
ZhengJY <zhengjunyuan@peilian.com>
2017-09-28 10:23:43 +0800

    gitignore修改

---
+++ backend/config/main-local.php
@@ -0,0 +1,26 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => '_HyqDvkmzv7JK4EPpASQvrYbq1LE3b1v',
+        ],
+    ],
+];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+        'allowedIPs' => ['127.0.0.1', '::1', '192.168.*.*', '192.168.178.20', '10.0.2.2'] // adjust this to your needs
+    ];
+}
+
+return $config;
